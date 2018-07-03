6c264c952f82d96f01c6e4213be438bcc819dd64
Daniel P. Clark <6ftdan@gmail.com>
2018-07-03 17:04:26 -0400

    Add rb_proc_new to rubysys
    Using `CallbackPtr` for parameter of `VALUE (*func)(ANYARGS)` since
    this is identical to the parameter used in `rb_define_method`.

--- src/rubysys/rproc.rs
+++ src/rubysys/rproc.rs
@@ -1,4 +1,4 @@
-use rubysys::types::{Argc, Value};
+use rubysys::types::{Argc, Value, CallbackPtr};
 
 extern "C" {
     pub fn rb_proc_call_with_block(rproc: Value,
@@ -6,4 +6,5 @@ extern "C" {
                                    argv: *const Value,
                                    pass_procval: Value)
                                    -> Value;
+    pub fn rb_proc_new(func: CallbackPtr, binding: Value) -> Value;
 }
